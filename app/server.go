76a9276a11402bb7ca77074dd073f73fa9904466
nishant42491 <rajadhyakshanishant@gmail.com>
2024-12-28 14:38:13 -0800

    second commit

--- app/server.go
+++ app/server.go
@@ -6,9 +6,37 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+func handleConnection(c net.Conn) {
+	// Read data from the connection
+
+	// Write data to the connection
+	// Close the connection
+
+	defer c.Close()
+
+	recievedData := make([]byte, 4096)
+	_, err := c.Read(recievedData)
+	if err != nil {
+		fmt.Println("Error reading data: ", err.Error())
+		return
+	}
+
+	fmt.Println("Data recieved: ", string(recievedData))
+
+	httpresponseheader := "HTTP/1.1 200 OK\r\n"
+
+	//write the response header
+	_, err = c.Write([]byte(httpresponseheader))
+
+	if err != nil {
+		fmt.Println("Error writing data: ", err.Error())
+		return
+	}
+
+	//print made connection
+	fmt.Println("Connection made")
+
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -19,12 +47,20 @@ func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("Error: ", err.Error())
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	//handle connection
+
+	fmt.Println("Connection accepted")
+
+	handleConnection(conn)
+
 }
